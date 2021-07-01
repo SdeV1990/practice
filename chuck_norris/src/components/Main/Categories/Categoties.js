97648cacb35fa91b9a8a0ebe1e14b81b6f04678c
SdeV1990 <sapemiaen@gmail.com>
2021-07-01 17:48:10 +0300

    Preliminary markup.

--- chuck_norris/src/components/Main/Categories/Categoties.js
+++ chuck_norris/src/components/Main/Categories/Categoties.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
+// Components
+import Category from './Category/Category'
+
 const Categories = () => {
+
+    // Array of categories
+    const categoriesEnumeration = ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"]
+
     return (
         <div className="categories-wrapper">
             <h1 className="categories-title">Categories</h1>
-            <div className="categories-names-container">Name 1</div>
+            <div className="categories-names-container">
+                {   
+                    categoriesEnumeration.map(category => {
+                        return (
+                            <Category category={category} key={"category-" + category}/>
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 }
--- chuck_norris/src/components/Main/Quote/Quote.js
+++ chuck_norris/src/components/Main/Quote/Quote.js
@@ -5,12 +5,10 @@ import React from 'react'
 const Quote = () => {
     return(
         <div className='quote-wrapper'>
-            <div className='quote-logo'>
-                Chuck image
-            </div>
-            <div className='quote-text'>
-                Some quote
-            </div>
+            <img className='quote-logo' alt="chuck_norris_great" src="/img/chuck_norris_great.png"/>
+            <p className='quote-text'>
+                The only thing Chuck Norris says on a job interview: “Hi, I’m Chuck Norris and I start tomorrow. Any questions?”
+            </p>
         </div>
     );
 }
